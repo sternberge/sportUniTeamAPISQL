a38c89ef58531683620f49775b13a6d3b933ce22
sternberge <aurelien.sternberger@gmail.com>
2017-12-29 11:20:36 +0100

    MAJ de la methode find de user, renvoie error si id n'existe pas. --> mettre a jours tous les autres controller

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -18,27 +18,52 @@ module.exports = {
           connection.release();
           return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));// Convertion de la réponse en format JSON
         }
-        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-        connection.release(); // CLOSE THE CONNECTION
+        else if (results.length > 0){
+          res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+          connection.release(); // CLOSE THE CONNECTION
+        }
+        else{
+          res.send(JSON.stringify({"status": 500, "error": "Id does not exist", "response": null}));
+          connection.release(); // CLOSE THE CONNECTION
+        }
       });
     });
   },
 
-  /*createUser(req, res, next) {
-    // rajouter le check si l'utilisateur n'est pas déja existant
-    //different type of check of the informations
-    req.checkBody('email','Email cannot be empty').notEmpty();
-    req.checkBody('email','Your email is not valid').isEmail();
-    req.checkBody('email','Your email should be between 4 and 100 characters').len(4,100);
-    req.checkBody('firstName','FirstName cannot be empty').notEmpty();
-    req.checkBody('lastName','Email cannot be empty').notEmpty();
-    req.checkBody('password','Your password should be between 8 and 100 characters').len(8,100);
-    req.checkBody('reEnterPassword','Your password is different').equals(req.body.password);
-    var errors = req.validationErrors();
-    if(errors){
-      res.send(errors);
-    }
-    else{
+
+
+  createUser(req, res, next){
+    module.exports.checkEmailUnicity(req.body.email)
+    .then(() => module.exports.createUserWithPromise(req, res, next))
+    .then(() => {
+      res.send(JSON.stringify({"status": 200, "error": null, "response": "User has been created"}));
+    })
+    .catch((error) => {
+      res.send(JSON.stringify({"status": 500, "error": error, "response": error}));
+    })
+  },
+
+  createUserWithPromise(req,res,next){
+
+    return new Promise(function (resolve, reject) {
+
+      // rajouter le check si l'utilisateur n'est pas déja existant
+      // different type of check of the informations
+      req.checkBody('email','Email cannot be empty').notEmpty();
+      req.checkBody('email','Your email is not valid').isEmail();
+      req.checkBody('email','Your email should be between 4 and 100 characters').len(4,100);
+      req.checkBody('firstName','FirstName cannot be empty').notEmpty();
+      req.checkBody('lastName','Email cannot be empty').notEmpty();
+      req.checkBody('password','Your password should be between 8 and 100 characters').len(8,100);
+      req.checkBody('reEnterPassword','Your password is different').equals(req.body.password);
+      var errors = req.validationErrors();
+
+      if(errors){
+        reject(errors);
+        return res.send(errors);
+      }
+
+      else{
         const firstName= req.body.firstName;
         const lastName=req.body.lastName;
         const gender= req.body.gender;
@@ -46,9 +71,11 @@ module.exports = {
         const password= req.body.password;
         const birthday= req.body.birthday;
         const userType= req.body.userType;
+
         db.pool.getConnection((error, connection) => {
           if (error){
-            return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+            reject(error);
+            return; // Pour sortir de la methode
           }
           //hash of the password and insert in the database
           bcrypt.hash(password, saltRounds, function(err, hash) {
@@ -57,144 +84,82 @@ module.exports = {
             [firstName,lastName,gender,email,hash,birthday,userType], (error, results, fields) => {
               if (error){
                 connection.release();
-                return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+                reject(error);
+                return; // pour sortir de la methode
               }
-              res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+              //res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
               connection.release(); // CLOSE THE CONNECTION
+              resolve(results.insertId);
             });
           });
         });
       }
-    },*/
+    })
+  },
 
-    createUser(req, res, next){
-      module.exports.checkEmailUnicity(req.body.email)
-      .then(() => module.exports.createUserWithPromise(req, res, next))
-      .then(() => {
-        res.send(JSON.stringify({"status": 200, "error": null, "response": "User has been created"}));
-      })
-      .catch((error) => {
-        res.send(JSON.stringify({"status": 500, "error": error, "response": error}));
-      })
-    },
-
-    createUserWithPromise(req,res,next){
-
-      return new Promise(function (resolve, reject) {
-
-        // rajouter le check si l'utilisateur n'est pas déja existant
-        // different type of check of the informations
-        req.checkBody('email','Email cannot be empty').notEmpty();
-        req.checkBody('email','Your email is not valid').isEmail();
-        req.checkBody('email','Your email should be between 4 and 100 characters').len(4,100);
-        req.checkBody('firstName','FirstName cannot be empty').notEmpty();
-        req.checkBody('lastName','Email cannot be empty').notEmpty();
-        req.checkBody('password','Your password should be between 8 and 100 characters').len(8,100);
-        req.checkBody('reEnterPassword','Your password is different').equals(req.body.password);
-        var errors = req.validationErrors();
-
-        if(errors){
-          reject(errors);
-          return res.send(errors);
-        }
-
-        else{
-            const firstName= req.body.firstName;
-            const lastName=req.body.lastName;
-            const gender= req.body.gender;
-            const email= req.body.email;
-            const password= req.body.password;
-            const birthday= req.body.birthday;
-            const userType= req.body.userType;
-
-            db.pool.getConnection((error, connection) => {
-              if (error){
-                reject(error);
-                return; // Pour sortir de la methode
-              }
-              //hash of the password and insert in the database
-              bcrypt.hash(password, saltRounds, function(err, hash) {
-                // Store hash in your password DB.
-                var query = connection.query('INSERT INTO Users (firstName,lastName,gender,email,password,birthday,userType) VALUES(?, ?, ?, ?, ?, ?, ?)',
-                [firstName,lastName,gender,email,hash,birthday,userType], (error, results, fields) => {
-                  if (error){
-                    connection.release();
-                    reject(error);
-                    return; // pour sortir de la methode
-                  }
-                  //res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-                  connection.release(); // CLOSE THE CONNECTION
-                  resolve(results.insertId);
-                });
-              });
-            });
-          }
-      })
-    },
-
-    editUser(req, res, next) {
-      const userId = req.params.user_id;
-      const userProperties = req.body;
-      db.pool.getConnection((error, connection) => {
-        const userProp = req.body;
+  editUser(req, res, next) {
+    const userId = req.params.user_id;
+    const userProperties = req.body;
+    db.pool.getConnection((error, connection) => {
+      const userProp = req.body;
+      if (error){
+        return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+      }
+      var query = connection.query('UPDATE Users SET ? WHERE userID = ?',[userProperties, userId], (error, results, fields) => {
         if (error){
+          connection.release();
           return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
         }
-        var query = connection.query('UPDATE Users SET ? WHERE userID = ?',[userProperties, userId], (error, results, fields) => {
-          if (error){
-            connection.release();
-            return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-          }
-          res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-          connection.release(); // CLOSE THE CONNECTION
-        });
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+        connection.release(); // CLOSE THE CONNECTION
       });
-    },
+    });
+  },
 
-    deleteUser(req, res, next) {
-      const userId = req.params.user_id;
-      db.pool.getConnection((error, connection) => {
+  deleteUser(req, res, next) {
+    const userId = req.params.user_id;
+    db.pool.getConnection((error, connection) => {
 
+      if (error){
+        return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+      }
+      var query = connection.query('DELETE FROM Users WHERE userID = ?', userId, (error, results, fields) => {
         if (error){
+          connection.release();
           return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
         }
-        var query = connection.query('DELETE FROM Users WHERE userID = ?', userId, (error, results, fields) => {
+        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+        connection.release(); // CLOSE THE CONNECTION
+      });
+    });
+  },
+
+  // check de l'email pour la création et l'update du User
+  checkEmailUnicity(email){
+    return new Promise((resolve,reject) => {
+      db.pool.getConnection((error, connection) => {
+        if (error){
+          reject(error);
+          //return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+        }
+        var query = connection.query('Select 1 from Users Where email = ?', email, (error, results, fields) => {
           if (error){
             connection.release();
-            return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-          }
-          res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-          connection.release(); // CLOSE THE CONNECTION
-        });
-      });
-    },
-
-    // check de l'email pour la création et l'update du User
-    checkEmailUnicity(email){
-      return new Promise((resolve,reject) => {
-        db.pool.getConnection((error, connection) => {
-          if (error){
             reject(error);
             //return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
           }
-          var query = connection.query('Select 1 from Users Where email = ?', email, (error, results, fields) => {
-            if (error){
-              connection.release();
-              reject(error);
-              //return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-            }
-            //res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-            connection.release(); // CLOSE THE CONNECTION
-            if(results.length > 0){
-              reject("Email already used");
+          //res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+          connection.release(); // CLOSE THE CONNECTION
+          if(results.length > 0){
+            reject("Email already used");
 
-            }
-            else{
-              resolve();
-            }
-          });
+          }
+          else{
+            resolve();
+          }
         });
       });
-    }
+    });
+  }
 
-  };
+};
