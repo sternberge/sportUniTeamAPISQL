23437a5d87e9a69b901cb2fe94b86fad09d7a4ac
sternberge <aurelien.sternberger@gmail.com>
2018-01-22 13:25:13 +0100

    Modification de la creation player --> ajout de ses trois classements unranked initiaux

--- controllers/player_controller.js
+++ controllers/player_controller.js
@@ -2,6 +2,7 @@ var db = require('./../db');
 const UserController = require('../controllers/user_controller');
 var expressValidator = require('express-validator');
 const RankRulesController = require('../controllers/rank_rules_controller');
+const SingleRankingController = require('../controllers/single_ranking_controller');
 
 module.exports = {
 
@@ -31,37 +32,74 @@ module.exports = {
 
 
 
-  createPlayer(req, res, next) {
-    // Check si l'email n'est pas deja dans la bdd
-    UserController.checkEmailUnicity(req.body.email)
-    //Creation du User
-    .then(() => UserController.createUserWithPromise(req, res, next))
-    //Creation du Player à l'aide du userId passé en parametre
-    .then((userId)=>{
-      return module.exports.insertPlayer(userId,req);
-    })
-    //
-    .then((response)=>{
-      //Recupere les derniers classements des 3 types
-      var rankingType = ["S","D","T"];
-      return Promise.all(rankingType.map((rank) => {
-        RankRulesController.getLastRankingPerType(rank);
-      }));
+  /*createPlayer(req, res, next) {
+  // Check si l'email n'est pas deja dans la bdd
+  UserController.checkEmailUnicity(req.body.email)
+  //Creation du User
+  .then(() => UserController.createUserWithPromise(req, res, next))
+  //Creation du Player à l'aide du userId passé en parametre
+  .then((userId)=>{
+  return module.exports.insertPlayer(userId,req);
+})
+//
+.then((response)=>{
+//Recupere les derniers classements des 3 types
+var rankingType = ["S","D","T"];
+Promise.all(rankingType.map((rank) => {
+RankRulesController.getLastRankingPerType(rank);
+})).then((response)=>{
+console.log(response);
+})
 
-    })
-    .then(function(response) {
-      console.log(response);
-      console.log("Second then");
-      res.send(JSON.stringify({"status": 200, "error": null, "response": response}));
-    })
-    //Les erreurs survenues plus haut sont catch ici
-    .catch((err) => {
-      res.send(JSON.stringify({"status": 500, "error": err, "response": null}));
-    });
-  },
+})
+.then(function(response) {
+console.log(response);
+console.log("Second then");
+res.send(JSON.stringify({"status": 200, "error": null, "response": response}));
+})
+//Les erreurs survenues plus haut sont catch ici
+.catch((err) => {
+res.send(JSON.stringify({"status": 500, "error": err, "response": null}));
+});
+},*/
 
-  insertPlayer(userId,req){
-    return new Promise((resolve,reject) => {
+async createPlayer(req,res,next) {
+  try{
+    //Check si l'email n'est pas deja existant
+    await UserController.checkEmailUnicity(req.body.email);
+    //Creation du profil utilisateur classique
+    const userId = await UserController.createUserWithPromise(req, res, next);
+    //creation du player grace au userId crée juste avant
+    const playerId = await module.exports.insertPlayer(userId,req);
+    console.log("Le joueur cree a le player Id :",playerId);
+    // On récupère le classement unranked pour le classements simples
+    var nonRankedValueSingle = await RankRulesController.getLastRankingPerType("S");
+    console.log("Valeur unranked pour classement simple :",nonRankedValueSingle);
+    //On cree 3 classements unranked Single pour le regional national et country
+    var type = ["R","N","C"];
+    const promisesPerType = type.map(type =>
+        SingleRankingController.createInitialRanking(nonRankedValueSingle,playerId,type)
+        .catch((error)=>{
+           console.log(error);
+         })
+    );
+    const resu = await Promise.all(promisesPerType);
+    res.send(JSON.stringify({"status": 200, "error": null, "response": "Player has been created"}));
+    // Promise.all(promisesPerType)
+    // .then(()=>{
+    //   console.log("Apres le all");
+    // })
+    //res.send("Player has been created");
+
+  }
+  catch(error){
+    //console.log(error);
+    res.sendStatus(200).send(error);
+  }
+},
+
+insertPlayer(userId,req){
+  return new Promise((resolve,reject) => {
     db.pool.getConnection((error, connection) => {
       //erreur de connection
       if (error){
@@ -85,98 +123,98 @@ module.exports = {
   });
 },
 
-  editPlayer(req, res, next) {
-    const playerId = req.params.player_id;
-    const playerProperties = req.body;
-    db.pool.getConnection((error, connection) => {
+editPlayer(req, res, next) {
+  const playerId = req.params.player_id;
+  const playerProperties = req.body;
+  db.pool.getConnection((error, connection) => {
+    if (error){
+      return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+    }
+    var query = connection.query('UPDATE Players SET ? WHERE PlayerID = ?',[playerProperties, playerId], (error, results, fields) => {
       if (error){
+        connection.release();
         return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
       }
-      var query = connection.query('UPDATE Players SET ? WHERE PlayerID = ?',[playerProperties, playerId], (error, results, fields) => {
-        if (error){
-          connection.release();
-          return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-        }
-        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-        connection.release(); // CLOSE THE CONNECTION
-      });
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      connection.release(); // CLOSE THE CONNECTION
     });
-  },
+  });
+},
 
-  deletePlayer(req, res, next) {
-    const playerId = req.params.player_id;
-    db.pool.getConnection((error, connection) => {
+deletePlayer(req, res, next) {
+  const playerId = req.params.player_id;
+  db.pool.getConnection((error, connection) => {
+    if (error){
+      return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+    }
+    var query = connection.query('DELETE FROM Players WHERE PlayerID = ?', playerId, (error, results, fields) => {
       if (error){
+        connection.release();
         return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
       }
-      var query = connection.query('DELETE FROM Players WHERE PlayerID = ?', playerId, (error, results, fields) => {
-        if (error){
-          connection.release();
-          return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-        }
-        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-        connection.release(); // CLOSE THE CONNECTION
-      });
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      connection.release(); // CLOSE THE CONNECTION
     });
-  },
+  });
+},
 
-  generateMyPlayerDropDownList(req, res, next){
-    const coachId = req.params.coach_id;
-    db.pool.getConnection((error, connection) => {
+generateMyPlayerDropDownList(req, res, next){
+  const coachId = req.params.coach_id;
+  db.pool.getConnection((error, connection) => {
 
+    if (error){
+      return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+    }
+    var query = connection.query('SELECT p.playerId, u.firstName,u.lastName FROM Players p inner join Teams t on p.Teams_teamId = t.teamId inner join Users u on u.userId = p.Users_userId WHERE teamId = (SELECT teamId FROM Teams WHERE Coaches_coachId = ? or Coaches_headCoachId = ?);',[coachId,coachId], (error, results, fields) => {
       if (error){
+        connection.release();
         return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
       }
-      var query = connection.query('SELECT p.playerId, u.firstName,u.lastName FROM Players p inner join Teams t on p.Teams_teamId = t.teamId inner join Users u on u.userId = p.Users_userId WHERE teamId = (SELECT teamId FROM Teams WHERE Coaches_coachId = ? or Coaches_headCoachId = ?);',[coachId,coachId], (error, results, fields) => {
-        if (error){
-          connection.release();
-          return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-        }
-        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-        connection.release(); // CLOSE THE CONNECTION
-      });
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      connection.release(); // CLOSE THE CONNECTION
     });
-  },
+  });
+},
 
-  generateOtherPlayerDropDownList(req, res, next){
-    const coachId = req.params.coach_id;
-    db.pool.getConnection((error, connection) => {
+generateOtherPlayerDropDownList(req, res, next){
+  const coachId = req.params.coach_id;
+  db.pool.getConnection((error, connection) => {
 
+    if (error){
+      return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+    }
+    var query = connection.query('SELECT p.playerId, u.firstName,u.lastName FROM Players p inner join Teams t on p.Teams_teamId = t.teamId inner join Users u on u.userId = p.Users_userId WHERE teamId != (SELECT teamId FROM Teams WHERE Coaches_coachId = ? or Coaches_headCoachId = ?);',[coachId,coachId], (error, results, fields) => {
       if (error){
+        connection.release();
         return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
       }
-      var query = connection.query('SELECT p.playerId, u.firstName,u.lastName FROM Players p inner join Teams t on p.Teams_teamId = t.teamId inner join Users u on u.userId = p.Users_userId WHERE teamId != (SELECT teamId FROM Teams WHERE Coaches_coachId = ? or Coaches_headCoachId = ?);',[coachId,coachId], (error, results, fields) => {
-        if (error){
-          connection.release();
-          return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-        }
-        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-        connection.release(); // CLOSE THE CONNECTION
-      });
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      connection.release(); // CLOSE THE CONNECTION
     });
-  },
+  });
+},
 
-  getPlayerNameFromId(req, res, next){
-    const playerId = req.params.playerId;
-    db.pool.getConnection((error, connection) => {
+getPlayerNameFromId(req, res, next){
+  const playerId = req.params.playerId;
+  db.pool.getConnection((error, connection) => {
 
+    if (error){
+      return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
+    }
+    var query = connection.query(`SELECT concat(u.firstName, ' ',u.lastName) as playerName FROM Players p INNER JOIN Users u on u.userId = p.Users_userId WHERE p.playerId = ?`,playerId, (error, results, fields) => {
       if (error){
+        connection.release();
         return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
       }
-      var query = connection.query(`SELECT concat(u.firstName, ' ',u.lastName) as playerName FROM Players p INNER JOIN Users u on u.userId = p.Users_userId WHERE p.playerId = ?`,playerId, (error, results, fields) => {
-        if (error){
-          connection.release();
-          return res.send(JSON.stringify({"status": 500, "error": error, "response": null}));
-        }
-        res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
-        connection.release(); // CLOSE THE CONNECTION
-      });
+      res.send(JSON.stringify({"status": 200, "error": null, "response": results}));
+      connection.release(); // CLOSE THE CONNECTION
     });
-  },
+  });
+},
 
 
-  getAllPlayerId(){
-    return new Promise((resolve,reject) => {
+getAllPlayerId(){
+  return new Promise((resolve,reject) => {
     db.pool.getConnection((error, connection) => {
 
       if (error){
@@ -192,5 +230,5 @@ module.exports = {
       });
     });
   });
-  }
+}
 };
