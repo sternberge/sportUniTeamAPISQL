f56453112bfc47eec595ad51ebc8a4797fc48334
Harry <showed@hotmail.fr>
2018-01-30 11:43:35 +0100

    Ajout de getCollegeNameFromCollegeId

--- routes/routes.js
+++ routes/routes.js
@@ -52,6 +52,8 @@ module.exports = (app) => {
   app.delete('/api/colleges/:college_id', CollegeController.deleteCollege);
   app.get('/api/colleges/:college_id', CollegeController.findCollegeById);
   app.get('/api/colleges/getCollegeNameFromTeamId/:teamId',CollegeController.getCollegeNameFromTeamId);
+    app.get('/api/colleges/getCollegeNameFromCollegeId/:collegeId',CollegeController.getCollegeNameFromCollegeId);
+
 
 
   //Coaches
